e76b0a332e195406abce8f0e969a4d96fab24334
ingvildforseth <ingvild-f@hotmail.com>
2016-03-04 13:54:13 +0100

    updated panel class with function to read buttons

--- class_panel.py
+++ class_panel.py
@@ -1,31 +1,31 @@
-import panel_driver
+#import panel_driver
 from threading import Thread
 from threading import Lock
 import time
-
+import PanelInterface
 
 class Panel:
 	def __init__(self):
 		self.button_list = []
 		self.mutex_key = Lock()
 		self.thread_started = False
-		self.polling_thread = Thread(target = self.polling_buttons, args = (),)
+		self.polling_thread = Thread(target = self.polling_panel, args = (),)
+		self.panelInterface = PanelInterface.PanelInterface()
 
-	def polling_buttons(self):
+	def polling_panel(self):
 		old_button = -1
 		self.thread_started = True
 		while True:
-			button = panel_driver.read_buttons()
+			button = self.read_panel()
 			if button >= 0 and button != old_button:
 				self.mutex_key.acquire()
 				self.button_list.append(button)	
 				self.mutex_key.release()
 				print (self.button_list)
 				old_button = button	
+	
 
-	#polling_thread = Thread(target = polling_buttons, args = (),)
-
-	def read_pressed_button(self):
+	def read_button(self):
 		if self.thread_started is not True:
 			self.start(self.polling_thread)
 		if self.button_list:
@@ -34,8 +34,22 @@ class Panel:
 			self.mutex_key.release()
 			#print first_element
 			return first_element
-		return 'no buttons pressed'	
-		
+		else:
+			pass
+			#return 'no buttons pressed'	
+
+
+	def read_panel(self):
+
+		for floor in range (0,4):
+			for button in range(0,3):
+				if (floor == 0 and button == 1) or (floor == 3 and button == 0):
+					pass
+				elif self.panelInterface.get_button_signal(button,floor):
+					#print 'floor %i, button %i' % (floor, button)
+					return (floor, button)
+
+			
 	def start(self,thread):
 		thread.daemon = True # Terminate thread when "main" is finished
 		thread.start()
