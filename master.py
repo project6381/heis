d7288230d1d93de44d94b2d92c5a78ca36d62b8f
Slappy <slappy@slapster.com>
2016-04-13 13:24:14 +0200

    ---and dont leave the master and slave behind

---
+++ master.py
@@ -0,0 +1,88 @@
+from master_handler import MasterHandler
+from message_handler import MessageHandler
+from ported_driver.constants import DIRN_DOWN, DIRN_UP, DIRN_STOP, N_FLOORS
+from config_parameters import SLAVE_TO_MASTER_PORT, MASTER_TO_SLAVE_PORT, N_ELEVATORS, MY_ID, TICK
+import time
+from collections import Counter
+import sys
+import subprocess
+
+def main():
+
+	
+	#instantiating classes
+	message_handler = MessageHandler()
+	master_handler = MasterHandler()
+	#downtime_send = time.time()
+	active_master = False
+	startup = True
+	startup_time = time.time() + 1
+	while startup:
+		print "startup"
+		time.sleep(0.1)
+		message_handler.send_to_slave([0 for floor in range(0,N_FLOORS)],[0 for floor in range(0,N_FLOORS)],N_ELEVATORS+1)
+		slave_message = message_handler.receive_from_slave()
+		if startup_time < time.time():
+			startup = False
+		if not message_handler.connected_to_network():
+			raise KeyboardInterrupt
+	
+	while True:
+		#try:
+			if not message_handler.connected_to_network():
+				raise KeyboardInterrupt
+			master_handler.update_master_alive(MY_ID)
+
+			if master_handler.check_master_alive() == MY_ID:
+				active_master = True
+			else:
+				slave_message = message_handler.receive_from_slave()
+
+				if slave_message is not None:
+					master_handler.update_elevator_online(slave_message['slave_id'])			
+
+			print "I am NOT master, my id is: " + str(MY_ID)
+
+			time.sleep(TICK)
+
+
+			while active_master:
+				if not message_handler.connected_to_network():
+					raise KeyboardInterrupt
+				master_handler.update_master_alive(MY_ID)
+
+				slave_message = message_handler.receive_from_slave()
+
+				if slave_message is not None:
+				
+					
+					master_handler.clear_completed_orders(slave_message['direction'], slave_message['last_floor'], slave_message['next_floor'])
+
+					master_handler.update_elevator_position(slave_message['slave_id'],slave_message['last_floor'],slave_message['next_floor'],slave_message['direction'])
+					
+					master_handler.add_new_orders(slave_message['slave_floor_up'],slave_message['slave_floor_down'])
+									
+					master_handler.update_sync_state(slave_message['slave_id'])
+					
+
+				(orders_up,orders_down) = master_handler.get_orders()
+
+				#if downtime_send < time.time():	
+				#	message_handler.send_to_slave(orders_up,orders_down,MY_ID,orders_id)
+				#	downtime_send = time.time() + 0.1
+				message_handler.send_to_slave(orders_up,orders_down,MY_ID)
+				if master_handler.check_master_alive() != MY_ID:
+					active_master = False
+
+				time.sleep(TICK)
+		
+		#except KeyboardInterrupt:
+		#	pass
+		#except StandardError as error:
+		#	print error
+		#finally:
+		#	print "exiting main"
+				
+
+if __name__ == "__main__":
+    main()
---
+++ slave.py
@@ -0,0 +1,82 @@
+from slave_driver import SlaveDriver
+from message_handler import MessageHandler
+from ported_driver.constants import N_FLOORS
+from config_parameters import SLAVE_TO_MASTER_PORT, MASTER_TO_SLAVE_PORT, MY_ID, TICK, N_ELEVATORS
+import time
+import sys
+import subprocess
+
+
+def main():
+
+	#time.sleep(4)
+
+	#instantiating classes
+	message_handler = MessageHandler()
+	slave_driver = SlaveDriver()
+	#orders_id = 0
+	#downtime_send = time.time()
+	orders_ok = True
+
+
+	while True:
+		#try:
+
+			position = slave_driver.read_position()
+			master_message = message_handler.receive_from_master()
+
+			if message_handler.no_active_master():
+				slave_driver.set_offline_mode(True)
+				#print "offline mode = True"
+			else:
+				slave_driver.set_offline_mode(False)
+				#print "offline mode = False"
+			
+			
+			if master_message is not None:	
+
+				#slave_driver.clear_floor_panel(master_message['orders_up'][:],master_message['orders_down'][:])
+						
+				#orders_id = master_message['orders_id']
+				#print str(orders_ok) + '    '+ str(master_message['master_id'])
+				if slave_driver.changing_master(master_message['master_id'],orders_ok):	
+					
+					(my_master_orders_up,my_master_orders_down) = slave_driver.read_saved_master_queue()
+					print "CHANGING MASTER STATE = TRUE -> my_master_queue: " + str(my_master_orders_up) + str(my_master_orders_down) + " master_message" + str(master_message['orders_up']) + str(master_message['orders_down'])
+					for floor in range(0,N_FLOORS):
+						if my_master_orders_up[floor] > 0:
+							my_master_orders_up[floor]=1
+						if my_master_orders_down[floor] > 0:
+							my_master_orders_down[floor]=1
+					message_handler.send_to_master(my_master_orders_up,my_master_orders_down,MY_ID,position[0],position[1],position[2])
+					orders_ok = True
+					
+					for floor in range(0,N_FLOORS):
+						if ( (my_master_orders_up[floor] > 0) and (master_message['orders_up'][floor] == 0) ) or ( (my_master_orders_down[floor] > 0) and (master_message['orders_down'][floor] == 0) ):
+							orders_ok = False 
+
+				else:
+
+
+					slave_driver.master_order_run_elevator(master_message['orders_up'][:],master_message['orders_down'][:])	
+
+					(floor_up,floor_down) = slave_driver.get_floor_panel()
+
+					move_timeout = slave_driver.read_move_timeout()
+			
+					if (move_timeout == False):
+						message_handler.send_to_master(floor_up,floor_down,MY_ID,position[0],position[1],position[2])
+						#downtime_send = time.time() + 0.1
+
+
+			time.sleep(TICK*N_ELEVATORS)
+
+		#except KeyboardInterrupt:
+		#	pass
+		#except StandardError as error:
+		#	print error
+		#finally:
+		#	print "exiting main"
+
+if __name__ == "__main__":
+    main()
