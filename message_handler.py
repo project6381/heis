10975499ab0f6045a61d3123edd7e510cffa0eb4
AT <god@heaven.tk>
2016-04-20 02:31:00 +0200

    Added network_coms

--- message_handler.py
+++ message_handler.py
@@ -1,260 +1,261 @@
 import time
-
-from socket import socket, AF_INET, SOCK_DGRAM, SOL_SOCKET, SO_BROADCAST
-from threading import Thread, Lock
 from thread import interrupt_main
+from threading import Thread, Lock
 
-import watchdogs
 import network_communication
-
-from ported_driver.constants import N_FLOORS
+import watchdogs
 from config_parameters import MASTER_TO_SLAVE_PORT, SLAVE_TO_MASTER_PORT, TICK, MASTER_TIMEOUT
+from ported_driver.constants import N_FLOORS
 
 
 class MessageHandler:
-	def __init__(self):
+    def __init__(self):
 
-		self.__slave_message = {'buttons_up': [0 for floor in range(0,N_FLOORS)],
-								'buttons_down': [0 for floor in range(0,N_FLOORS)],
-								'slave_id': 0,
-								'last_floor': 0,
-								'next_floor': 0,
-								'direction': 0}
+        self.__slave_message = {'buttons_up': [0 for floor in range(0, N_FLOORS)],
+                                'buttons_down': [0 for floor in range(0, N_FLOORS)],
+                                'slave_id': 0,
+                                'last_floor': 0,
+                                'next_floor': 0,
+                                'direction': 0}
 
-		self.__master_message = {'orders_up': [0 for floor in range(0,N_FLOORS)],
-								'orders_down': [0 for floor in range(0,N_FLOORS)],
-								'master_id': 0}
+        self.__master_message = {'orders_up': [0 for floor in range(0, N_FLOORS)],
+                                 'orders_down': [0 for floor in range(0, N_FLOORS)],
+                                 'master_id': 0}
 
-		self.__receive_buffer_slave = [] 
-		self.__receive_buffer_master = [] 	
-		
-		self.__receive_buffer_slave_key = Lock()
-		self.__receive_buffer_master_key = Lock()
-		self.__is_connected_to_network_key = Lock()
-		self.__no_active_master_key = Lock()
+        self.__receive_buffer_slave = []
+        self.__receive_buffer_master = []
 
-		self.__no_active_master = False
-		self.__buffering_master_messages_thread_started = False
-		self.__buffering_slave_messages_thread_started = False
-		self.__master_alive_thread_started = False
-		self.__is_connected_to_network = False
+        self.__receive_buffer_slave_key = Lock()
+        self.__receive_buffer_master_key = Lock()
+        self.__is_connected_to_network_key = Lock()
+        self.__no_active_master_key = Lock()
 
-		self.__thread_buffering_master_messages = Thread(target = self.__buffering_master_messages_thread, args = (), name = "MessageHandler.__buffering_master_messages_thread")
-		self.__thread_buffering_slave_messages = Thread(target = self.__buffering_slave_messages_thread, args = (),name = "MessageHandler.__buffering_slave_messages_thread")
-		self.__thread_master_alive = Thread(target = self.__master_alive_thread, args = (),name = "MessageHandler.__master_alive_thread")
+        self.__no_active_master = False
+        self.__buffering_master_messages_thread_started = False
+        self.__buffering_slave_messages_thread_started = False
+        self.__master_alive_thread_started = False
+        self.__is_connected_to_network = False
 
-		self.__timeout_no_active_master = time.time() + MASTER_TIMEOUT
+        self.__thread_buffering_master_messages = Thread(target=self.__buffering_master_messages_thread, args=(),
+                                                         name="MessageHandler.__buffering_master_messages_thread")
+        self.__thread_buffering_slave_messages = Thread(target=self.__buffering_slave_messages_thread, args=(),
+                                                        name="MessageHandler.__buffering_slave_messages_thread")
+        self.__thread_master_alive = Thread(target=self.__master_alive_thread, args=(),
+                                            name="MessageHandler.__master_alive_thread")
 
-	def no_active_master(self):
-		if self.__master_alive_thread_started is not True:
-			self.__start_thread(self.__thread_master_alive)
+        self.__timeout_no_active_master = time.time() + MASTER_TIMEOUT
 
-		with self.__no_active_master_key: 
-			return self.__no_active_master
+    def no_active_master(self):
+        if self.__master_alive_thread_started is not True:
+            self.__start_thread(self.__thread_master_alive)
 
-	def connected_to_network(self):
-		with self.__is_connected_to_network_key:
-			return self.__is_connected_to_network
+        with self.__no_active_master_key:
+            return self.__no_active_master
 
-	def send_to_master(self,buttons_up,buttons_down,slave_id,last_floor,next_floor,direction):
-		message = str()
+    def connected_to_network(self):
+        with self.__is_connected_to_network_key:
+            return self.__is_connected_to_network
 
-		for button in buttons_up:
-			message += str(button)	
+    def send_to_master(self, buttons_up, buttons_down, slave_id, last_floor, next_floor, direction):
+        message = str()
 
-		for button in buttons_down:
-			message += str(button)
+        for button in buttons_up:
+            message += str(button)
 
-		message += str(slave_id)
-		message += str(last_floor)
-		message += str(next_floor)
-		message += str(direction)
+        for button in buttons_down:
+            message += str(button)
 
-		self.__send(message,SLAVE_TO_MASTER_PORT)
+        message += str(slave_id)
+        message += str(last_floor)
+        message += str(next_floor)
+        message += str(direction)
 
-	def send_to_slave(self,orders_up,orders_down,master_id):
-		message = str()
-		
-		for order in orders_up:
-			message += str(order)
+        self.__send(message, SLAVE_TO_MASTER_PORT)
 
-		for order in orders_down:
-			message += str(order)	
+    def send_to_slave(self, orders_up, orders_down, master_id):
+        message = str()
 
-		message += str(master_id)
-		
-		self.__send(message,MASTER_TO_SLAVE_PORT)
-			
-	def receive_from_master(self):
-		message = self.__read_message(MASTER_TO_SLAVE_PORT)
+        for order in orders_up:
+            message += str(order)
 
-		if message is not None:
+        for order in orders_down:
+            message += str(order)
 
-			for floor in range (0,N_FLOORS):
-				self.__master_message['orders_up'][floor] = int(message[floor])
-				self.__master_message['orders_down'][floor] = int(message[N_FLOORS+floor])
+        message += str(master_id)
 
-			self.__master_message['master_id'] = int(message[N_FLOORS*2])
+        self.__send(message, MASTER_TO_SLAVE_PORT)
 
-			return self.__master_message
+    def receive_from_master(self):
+        message = self.__read_message(MASTER_TO_SLAVE_PORT)
 
-	def receive_from_slave(self):				
-		message = self.__read_message(SLAVE_TO_MASTER_PORT)
-		
-		if message is not None:
+        if message is not None:
 
-			for floor in range (0,N_FLOORS):
-					self.__slave_message['buttons_up'][floor] = int(message[floor])
-					self.__slave_message['buttons_down'][floor] = int(message[N_FLOORS+floor]) 	
+            for floor in range(0, N_FLOORS):
+                self.__master_message['orders_up'][floor] = int(message[floor])
+                self.__master_message['orders_down'][floor] = int(message[N_FLOORS + floor])
 
-			self.__slave_message['slave_id'] = int(message[N_FLOORS*2])
-			self.__slave_message['last_floor'] = int(message[N_FLOORS*2 + 1])
-			self.__slave_message['next_floor'] = int(message[N_FLOORS*2 + 2])
-			self.__slave_message['direction'] = int(message[N_FLOORS*2 + 3])
+            self.__master_message['master_id'] = int(message[N_FLOORS * 2])
 
-			return self.__slave_message
+            return self.__master_message
 
-	def __send(self, data, port):
-		try:
-			broadcaster = network_communication.Broadcaster(port)
-			broadcaster.broadcast(data)
+    def receive_from_slave(self):
+        message = self.__read_message(SLAVE_TO_MASTER_PORT)
 
-			##### NO EXCEPTION RAISED ABOVE IS TAKEN TO IMPLY CONNECTION TO NETWORK ######
-			with self.__is_connected_to_network_key:
-				self.__is_connected_to_network = True
+        if message is not None:
 
-		except IOError as error:
-			with self.__is_connected_to_network_key:
-				self.__is_connected_to_network = False
+            for floor in range(0, N_FLOORS):
+                self.__slave_message['buttons_up'][floor] = int(message[floor])
+                self.__slave_message['buttons_down'][floor] = int(message[N_FLOORS + floor])
 
-			print error
-			print "MessageHandler.__send: Failed. Network down?"
-			print "Sleeping 1 sec.."
-			time.sleep(1)
+            self.__slave_message['slave_id'] = int(message[N_FLOORS * 2])
+            self.__slave_message['last_floor'] = int(message[N_FLOORS * 2 + 1])
+            self.__slave_message['next_floor'] = int(message[N_FLOORS * 2 + 2])
+            self.__slave_message['direction'] = int(message[N_FLOORS * 2 + 3])
 
-	def __read_message(self,port):			
-			if port == MASTER_TO_SLAVE_PORT:
-				if self.__buffering_slave_messages_thread_started is not True:
-					self.__start_thread(self.__thread_buffering_slave_messages)
-				
-				with self.__receive_buffer_slave_key:
-					if self.__receive_buffer_slave: 
-						return self.__receive_buffer_slave.pop(0)
-					else:
-						return None
+            return self.__slave_message
 
-			elif port == SLAVE_TO_MASTER_PORT: 
-				if self.__buffering_master_messages_thread_started is not True:
-					self.__start_thread(self.__thread_buffering_master_messages)
+    def __send(self, data, port):
+        try:
+            broadcaster = network_communication.Broadcaster(port)
+            broadcaster.broadcast(data)
 
-				with self.__receive_buffer_master_key:
-					if self.__receive_buffer_master: 
-						return self.__receive_buffer_master.pop(0)
-					else:
-						return None
+            ##### NO EXCEPTION RAISED ABOVE IS TAKEN TO IMPLY CONNECTION TO NETWORK ######
+            with self.__is_connected_to_network_key:
+                self.__is_connected_to_network = True
 
-	def __start_thread(self,thread):
-			try:
-				thread.daemon = True
-				thread.start()
+        except IOError as error:
+            with self.__is_connected_to_network_key:
+                self.__is_connected_to_network = False
 
-			except StandardError as error:
-				print error
-				print "MessageHandler.__start_thread(): Thread: %s operation failed" % (thread.name)
-				interrupt_main()
+            print error
+            print "MessageHandler.__send: Failed. Network down?"
+            print "Sleeping 1 sec.."
+            time.sleep(1)
 
-	def __buffering_master_messages_thread(self):
-		try:
-			self.__buffering_master_messages_thread_started = True
+    def __read_message(self, port):
+        if port == MASTER_TO_SLAVE_PORT:
+            if self.__buffering_slave_messages_thread_started is not True:
+                self.__start_thread(self.__thread_buffering_slave_messages)
 
-			try:
-				master_message_receiver = network_communication.Receiver(SLAVE_TO_MASTER_PORT)
+            with self.__receive_buffer_slave_key:
+                if self.__receive_buffer_slave:
+                    return self.__receive_buffer_slave.pop(0)
+                else:
+                    return None
 
-			except IOError as error:
-				print error
-				print "MessageHandler.__buffering_master_messages_thread:"
-				print "Failed setting up udp sockets"
-				interrupt_main()
+        elif port == SLAVE_TO_MASTER_PORT:
+            if self.__buffering_master_messages_thread_started is not True:
+                self.__start_thread(self.__thread_buffering_master_messages)
 
-			while True:
-				try:
-					message = master_message_receiver.wait_for_and_receive_broadcast()
+            with self.__receive_buffer_master_key:
+                if self.__receive_buffer_master:
+                    return self.__receive_buffer_master.pop(0)
+                else:
+                    return None
 
-				except IOError as error:
-					print error
-					print "MessageHandler.__buffering_master_messages_thread:"
-					print "Receive failed!"
-					print "Sleeping 1 sec.."
-					time.sleep(1)
-				
-				if message is not None:
-					with self.__receive_buffer_master_key:
-						if message in self.__receive_buffer_master:
-							pass
-						else:
-							self.__receive_buffer_master.append(message)
+    def __start_thread(self, thread):
+        try:
+            thread.daemon = True
+            thread.start()
 
-		except StandardError as error:
-			print error
-			print "MessageHandler.__buffering_master_messages"
-			interrupt_main()
+        except StandardError as error:
+            print error
+            print "MessageHandler.__start_thread(): Thread: %s operation failed" % (thread.name)
+            interrupt_main()
 
-	def __buffering_slave_messages_thread(self):
-		try:
-			self.__buffering_slave_messages_thread_started = True
+    def __buffering_master_messages_thread(self):
+        try:
+            self.__buffering_master_messages_thread_started = True
 
-			try:
-				slave_message_receiver = network_communication.Receiver(MASTER_TO_SLAVE_PORT)
+            try:
+                master_message_receiver = network_communication.Receiver(SLAVE_TO_MASTER_PORT)
 
-			except IOError as error:
-				print error
-				print "MessageHandler.__buffering_slave_messages_thread:"
-				print "Failed setting up udp sockets"
-				interrupt_main()
-			
-			while True:
-				try:
-					message = slave_message_receiver.wait_for_and_receive_broadcast()
+            except IOError as error:
+                print error
+                print "MessageHandler.__buffering_master_messages_thread:"
+                print "Failed setting up udp sockets"
+                interrupt_main()
 
-				except IOError as error:
-					print error
-					print "MessageHandler.__buffering_slave_messages_thread:"
-					print "Receive failed!"
-					print "Sleeping 1 sec.."
-					time.sleep(1)
+            while True:
+                try:
+                    message = master_message_receiver.wait_for_and_receive_broadcast()
 
-				if message is not None:
-					self.__timeout_no_active_master = time.time() + MASTER_TIMEOUT
-					with self.__no_active_master_key:
-						self.__no_active_master = False
+                except IOError as error:
+                    print error
+                    print "MessageHandler.__buffering_master_messages_thread:"
+                    print "Receive failed!"
+                    print "Sleeping 1 sec.."
+                    time.sleep(1)
 
-					with self.__receive_buffer_slave_key:
-						if message in self.__receive_buffer_slave:
-							pass
-						else:	
-							self.__receive_buffer_slave.append(message)
+                if message is not None:
+                    with self.__receive_buffer_master_key:
+                        if message in self.__receive_buffer_master:
+                            pass
+                        else:
+                            self.__receive_buffer_master.append(message)
 
-		except StandardError as error:
-			print error
-			print "MessageHandler.__buffering_slave_messages"
-			interrupt_main()
+        except StandardError as error:
+            print error
+            print "MessageHandler.__buffering_master_messages"
+            interrupt_main()
 
-	def __master_alive_thread(self):
-		try:
-			__master_alive_thread_watchdog = watchdogs.ThreadWatchdog(1,"watchdog event: MessageHandler.__master_alive_thread")
-			__master_alive_thread_watchdog.start_watchdog()
+    def __buffering_slave_messages_thread(self):
+        try:
+            self.__buffering_slave_messages_thread_started = True
 
-			self.__master_alive_thread_started = True
+            try:
+                slave_message_receiver = network_communication.Receiver(MASTER_TO_SLAVE_PORT)
 
-			while True:				
-				time.sleep(TICK*100)
-				__master_alive_thread_watchdog.pet_watchdog()
+            except IOError as error:
+                print error
+                print "MessageHandler.__buffering_slave_messages_thread:"
+                print "Failed setting up udp sockets"
+                interrupt_main()
 
-				if self.__timeout_no_active_master < time.time():
-					with self.__no_active_master_key:
-						self.__no_active_master = True
+            while True:
+                try:
+                    message = slave_message_receiver.wait_for_and_receive_broadcast()
 
-		except StandardError as error:
-			print error
-			print "MessageHandler.__master_alive"
-			interrupt_main()
+                except IOError as error:
+                    print error
+                    print "MessageHandler.__buffering_slave_messages_thread:"
+                    print "Receive failed!"
+                    print "Sleeping 1 sec.."
+                    time.sleep(1)
+
+                if message is not None:
+                    self.__timeout_no_active_master = time.time() + MASTER_TIMEOUT
+                    with self.__no_active_master_key:
+                        self.__no_active_master = False
+
+                    with self.__receive_buffer_slave_key:
+                        if message in self.__receive_buffer_slave:
+                            pass
+                        else:
+                            self.__receive_buffer_slave.append(message)
+
+        except StandardError as error:
+            print error
+            print "MessageHandler.__buffering_slave_messages"
+            interrupt_main()
+
+    def __master_alive_thread(self):
+        try:
+            __master_alive_thread_watchdog = watchdogs.ThreadWatchdog(1,
+                                                                      "watchdog event: MessageHandler.__master_alive_thread")
+            __master_alive_thread_watchdog.start_watchdog()
+
+            self.__master_alive_thread_started = True
+
+            while True:
+                time.sleep(TICK * 100)
+                __master_alive_thread_watchdog.pet_watchdog()
+
+                if self.__timeout_no_active_master < time.time():
+                    with self.__no_active_master_key:
+                        self.__no_active_master = True
+
+        except StandardError as error:
+            print error
+            print "MessageHandler.__master_alive"
+            interrupt_main()
--- network_communication.py
+++ network_communication.py
@@ -2,83 +2,82 @@ from socket import socket, AF_INET, SOCK_DGRAM, SOL_SOCKET, SO_BROADCAST
 
 
 class Receiver(object):
-	
-	def __init__(self, port):
-		self.__bind_port = ('', port)
-		self.__udp = socket(AF_INET, SOCK_DGRAM)
-		self.__udp.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-		self.__udp.bind(self.__bind_port)
+    def __init__(self, port):
+        self.__bind_port = ('', port)
+        self.__udp = socket(AF_INET, SOCK_DGRAM)
+        self.__udp.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+        self.__udp.bind(self.__bind_port)
 
-	def wait_for_and_receive_broadcast(self):
-		data, address = self.__udp.recvfrom(1024)
-		return self.__errorcheck_and_remove_formatting(data)
+    def wait_for_and_receive_broadcast(self):
+        data, address = self.__udp.recvfrom(1024)
+        return self.__errorcheck_and_remove_formatting(data)
 
-	def __errorcheck_and_remove_formatting(self,data):
-		###### CHECKS THAT THE MESSAGE HAS THE FORMAT '<provided_message_length;message>' ######
-		if (data[0] == '<') and (data[len(data) -1] == '>'):
-			character_counter = 1
-			separator_found = False
-			separator_position = 0
+    def __errorcheck_and_remove_formatting(self, data):
+        ###### CHECKS THAT THE MESSAGE HAS THE FORMAT '<provided_message_length;message>' ######
+        if (data[0] == '<') and (data[len(data) - 1] == '>'):
+            character_counter = 1
+            separator_found = False
+            separator_position = 0
 
-			for character in data:
-				if (character == ";") and (separator_found == False):
-					separator_position = character_counter
-					separator_found = True
-				character_counter += 1
+            for character in data:
+                if (character == ";") and (separator_found == False):
+                    separator_position = character_counter
+                    separator_found = True
+                character_counter += 1
 
-			measured_message_length = str(len(data) - separator_position - 1)
-			provided_message_length = str()
+            measured_message_length = str(len(data) - separator_position - 1)
+            provided_message_length = str()
 
-			for character in range(1, separator_position - 1):
-				provided_message_length += data[character]
+            for character in range(1, separator_position - 1):
+                provided_message_length += data[character]
 
-			if (provided_message_length == measured_message_length) and (separator_found == True):
-				message = str()
-				for character in range(separator_position,len(data) - 1):
-					message += data[character]
-				return message
-			else:
-				return None
-		else:
-			return None
+            if (provided_message_length == measured_message_length) and (separator_found == True):
+                message = str()
+                for character in range(separator_position, len(data) - 1):
+                    message += data[character]
+                return message
+            else:
+                return None
+        else:
+            return None
+
 
 class Broadcaster(object):
+    def __init__(self, port):
+        self.__send_flags = ('<broadcast>', port)
 
-	def __init__(self, port):
-		self.__send_flags = ('<broadcast>', port)
+    def broadcast(self, data):
+        broadcast_data = self.__format_data_for_errorchecking(data)
 
-	def broadcast(self, data):
-		broadcast_data = self.__format_data_for_errorchecking(data)
+        with BroadcastSocketUDP() as udp:
+            udp.sendto(broadcast_data, self.__send_flags)
 
-		with BroadcastSocketUDP() as udp:
-			udp.sendto(broadcast_data, self.__send_flags)
+    def __format_data_for_errorchecking(self, data):
+        ###### MESSAGE IN FORMAT '<message_length;message>' FOR ERRORCHECK ######
+        return '<%s;%s>' % (str(len(data)), data)
 
-	def __format_data_for_errorchecking(self, data):
-		###### MESSAGE IN FORMAT '<message_length;message>' FOR ERRORCHECK ######
-		return '<%s;%s>' % (str(len(data)), data)
 
 class BroadcastSocketUDP(object):
-	
-	def __init__(self):
-		self.udp = socket(AF_INET, SOCK_DGRAM)
-		self.udp.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+    def __init__(self):
+        self.udp = socket(AF_INET, SOCK_DGRAM)
+        self.udp.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 
-	def __enter__(self):
-		return self.udp
+    def __enter__(self):
+        return self.udp
 
-	def __exit__(self, type, value, traceback):
-		self.udp.close()
+    def __exit__(self, type, value, traceback):
+        self.udp.close()
+
 
 class ReceiverSocketUDP(object):
+    def __init__(self, port):
+        self.__bind_port = ('', port)
+        self.__udp = socket(AF_INET, SOCK_DGRAM)
+        self.__udp.bind(self.__bind_port)
+        self.__udp.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 
-	def __init__(self, port):
-		self.__bind_port = ('', port)
-		self.__udp = socket(AF_INET, SOCK_DGRAM)
-		self.__udp.bind(self.__bind_port)
-		self.__udp.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-		
-	def __enter__(self):
-		return self.__udp
+    def __enter__(self):
+        return self.__udp
 
-	def __exit__(self, type, value, traceback):
-		self.__udp.close()
+    def __exit__(self, type, value, traceback):
+        self.__udp.close()
